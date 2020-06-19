19c91ab27e102bc671a18e1d840d4adf9ea4345c
Chris Anderson <chrisanderson@chriss-air.lan>
2020-06-18 21:30:32 -0700

    progress!

--- thiel.py
+++ thiel.py
@@ -40,28 +40,29 @@ DATA_DIR = join(dirname(__file__), 'datalogs')
 
 DEFAULT_FIELDS = ['XY', 'LatLon', 'VxVy']
 
+simname = 'sim2'
+realname = 'real2'
+
 def nix(val, lst):
     return [x for x in lst if x != val]
 
 @lru_cache()
-def load_data(data_type):
-    fname = join(DATA_DIR, '%s.csv' % data_type)  # fix this later
+def load_data(name):
+    fname = join(DATA_DIR, '%s.csv' % name)  # fix this later
     data = pd.read_csv(fname)
     return pd.DataFrame(data)
-#    return pd.DataFrame({data_type: data.y})
 
 @lru_cache()
-def get_data():
-    dfsim = load_data('sim2')
-#    print("sim",dfsim.y)
-    dfreal = load_data('real2')
-#    print("real",dfreal.y)
-#    data = pd.concat([dfsim, dfreal], keys=['dfsim', 'dfreal'], names=['Sim', 'Real'], ignore_index = True, axis=1)
-#    data = pd.concat([dfsim, dfreal], axis=1)
-#    data = data.dropna()   # remove missing values
-    dfsim = pd.concat([dfsim.simx,dfsim.simy], axis=1)
-    dfreal = pd.concat([dfreal.realx,dfreal.realy], axis=1)
-    return dfsim,dfreal
+def get_data(simname,realname):
+    dfsim = load_data(simname)
+    dfreal = load_data(realname)
+    data = pd.concat([dfsim, dfreal], axis=1)
+    data = data.dropna()   # remove missing values
+    data['simy'] = data.simy
+    data['simx'] = data.simx
+    data['realy'] = data.realy
+    data['realx'] = data.realx
+    return data
 
 # set up widgets
 
@@ -71,11 +72,12 @@ real = Select(value='VxVy', options=nix('XY', DEFAULT_FIELDS))
 
 # set up plots
 
-dfsim, dfreal = get_data()
-simsource = ColumnDataSource(dfsim)
-simsource_static = ColumnDataSource(dfsim)
-realsource = ColumnDataSource(dfreal)
-realsource_static = ColumnDataSource(dfreal)
+source = ColumnDataSource(data = dict(simx=[],simy=[],realx=[],realy=[]))
+source_static = ColumnDataSource(data = dict(simx=[],simy=[],realx=[],realy=[]))
+##simsource = ColumnDataSource(dfsim)
+##simsource_static = ColumnDataSource(dfsim)
+##realsource = ColumnDataSource(dfreal)
+##realsource_static = ColumnDataSource(dfreal)
 
 tools = 'pan,wheel_zoom,xbox_select,reset'
 
@@ -83,13 +85,13 @@ tools = 'pan,wheel_zoom,xbox_select,reset'
 #corr.circle('Sim X', 'Real X', size=2, source=simsource, selection_color="orange", alpha=0.6, nonselection_alpha=0.1, selection_alpha=0.4)
 
 ts1 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='linear', active_drag="xbox_select")
-ts1.line('simx', 'simy', source=simsource_static)
-ts1.circle('simx', 'simy', size=1, source=simsource, color=None, selection_color="orange")
+ts1.line('simx', 'simy', source=source_static)
+ts1.circle('simx', 'simy', size=1, source=source, color=None, selection_color="orange")
 
 ts2 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='linear', active_drag="xbox_select")
 ts2.x_range = ts1.x_range
-ts2.line('realx', 'realy', source=realsource_static)
-ts2.circle('realx', 'realy', size=1, source=realsource, color=None, selection_color="orange")
+ts2.line('realx', 'realy', source=source_static)
+ts2.circle('realx', 'realy', size=1, source=source, color=None, selection_color="orange")
 
 # set up callbacks
 
@@ -102,37 +104,27 @@ def real_change(attrname, old, new):
     update()
 
 def update(selected=None):
-    global simsource, source_static, realsource, realsource_static
-    dfsim, dfreal = get_data()
-    print("sim", sim)
-    print("real",real)
-    data = pd.concat([dfsim, dfreal], axis=1)
-    print(data)
-    simsource = dfsim
-    simsource_static = dfsim
-    realsource = dfreal
-    realsource_static = dfreal
-
-#    update_stats(data, sim, real)
-
-#    corr.title.text = 'Sim vs Real'
+    tempdata = get_data(simname,realname)
+    data = tempdata[['simx', 'simy','realx','realy']]
+    source.data = data
+    source_static.data = data
     ts1.title.text, ts2.title.text = 'Sim', 'Real'
 
-def update_stats(data, sim, real):
-    stats.text = str(data[[df1.y, df2.y, 'Sim', 'Real']].describe())
+##def update_stats(data, sim, real):
+##    stats.text = str(data[[df1.y, df2.y, 'Sim', 'Real']].describe())
 
 sim.on_change('value', sim_change)
 real.on_change('value', real_change)
 
 def selection_change(attrname, old, new):
 #    sim, real = sim.value, real.value
-    data = get_data()
-    selected = simsource.selected.indices
+    data = get_data(simname,realname)
+    selected = source.selected.indices
     if selected:
         data = data.iloc[selected, :]
 #    update_stats(data, sim, real)
     
-simsource.selected.on_change('indices', selection_change)
+source.selected.on_change('indices', selection_change)
     
 #file_input = FileInput(accept=".ulg, .csv")
 #file_input.on_change('value', get_data())
@@ -144,7 +136,7 @@ sim_upload_text = Paragraph(text="Upload a simulator datalog:",width=500, height
 real_upload_text = Paragraph(text="Upload a corresponding real-world datalog:",width=500, height=15)
 #checkbox_group = CheckboxGroup(labels=["x", "y", "vx","vy","lat","lon"], active=[0, 1])
 
-simsource.selected.on_change('indices', selection_change)
+source.selected.on_change('indices', selection_change)
 
 # set up layout
 widgets = column(sim, real)
